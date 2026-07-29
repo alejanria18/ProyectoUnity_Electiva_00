12144edb66f7b74d189b332e15ee45233ac018d3
Julian1091 <jlancheros2004@gmail.com>
2026-07-28 21:46:03 -0500

    Update RegistroGrupo.cs
    Inicio	del	script	RegistroGrupo.cs	por	Julian Andres Lancheros Andrade

--- ProyectoUnityElectiva00/Assets/Scripts/RegistroGrupo.cs
+++ ProyectoUnityElectiva00/Assets/Scripts/RegistroGrupo.cs
@@ -2,6 +2,6 @@
 //	Fecha:	[27/07/2026]
 //	Hora:	[13:22]
 
-//	Nombre	completo:	[Nombre	Estudiante]
-//	Fecha:	[DD/MM/AAAA]
-//	Hora:	[HH:MM]
+//	Nombre	completo: Julian Andres Lancheros Andrade
+//	Fecha:	[28/07/2026]
+//	Hora:	[21:42]
