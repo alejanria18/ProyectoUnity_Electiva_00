558c097408d26ba8a681f86023643ea9dfa995fb
Santiago Castillo <71842282+SantiCP13@users.noreply.github.com>
2026-07-29 15:04:38 -0500

    Aporte en RegistroGrupo.cs por [Santiago Castillo]

--- ProyectoUnityElectiva00/Assets/Scripts/RegistroGrupo.cs
+++ ProyectoUnityElectiva00/Assets/Scripts/RegistroGrupo.cs
@@ -1,7 +1,11 @@
-//	Nombre	completo: Paula Alejandra Riaño Jimenez
+//	Nombre	completo: Paula Alejandra RiaÃ±o Jimenez
 //	Fecha:	[27/07/2026]
 //	Hora:	[13:22]
 
 //	Nombre	completo: Julian Andres Lancheros Andrade
 //	Fecha:	[28/07/2026]
 //	Hora:	[21:42]
+
+// Nombre completo: Santiago Castillo
+// Fecha: 29/07/2026
+// Hora: 15:00
